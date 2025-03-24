0ed7e7d4ef056db80e8fbeb6b5924366dc6f4b59
Ruslan <roslavich16@gmail.com>
2025-03-24 20:08:23 +0200

    оператор сравнения

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-let userName = "Ruslan"
-let age = 33
-console.log(userName + " is " + age)
+let age = 33;
+if (age>=18) {
+    console.log("Вы совершеннолетний.");
+} else {
+    console.log("Вы несовершеннолетний.");
+}
