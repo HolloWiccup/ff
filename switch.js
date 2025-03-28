e8133a81c8531068e7fa433c06dbd47327802fdf
Ruslan <roslavich16@gmail.com>
2025-03-28 22:03:58 +0200

    switch + тест добавления одного файла

---
+++ switch.js
@@ -0,0 +1,13 @@
+switch (0) {
+  case 0:
+    console.log("холодно");
+    break;
+  case 15:
+    console.log("свежо");
+    break;
+  case 30:
+    console.log("жара");
+    break;
+  default:
+    console.log("хз");
+}
