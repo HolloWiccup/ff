9a8cc016b2c81c11c39cf64628e603beb973b0c3
Ruslan <roslavich16@gmail.com>
2025-04-05 14:23:33 +0300

    Вынес экспериментальные массивы отедльно в модуль

---
+++ app.js
@@ -0,0 +1,15 @@
+export const todoList = [
+  "купить BMW",
+  "выучить JS",
+  "полететь в космос",
+  "запилить дверь",
+];
+export const newArray = [1, 11, -2, 3, -10, 4];
+
+export const todoListMassiv = [
+  { task: "купить BMW", status: "В процессе" },
+  { task: "выучить JS", status: "Нужно сделать" },
+  { task: "полететь в космос", status: "Готов" },
+  { task: "запилить дверь", status: "Нужно сделать" },
+];
+export const newArray2 = [1, 11, -2, 3, -10, 4];
