a270cd917c5205c3c920b2e11d663a50e5e945d6
Ruslan <roslavich16@gmail.com>
2025-04-14 20:35:33 +0300

    добавил паузу в кнопку старт

--- CounterUI/main.js
+++ CounterUI/main.js
@@ -3,21 +3,26 @@ const stop = document.getElementById("stop");
 let result = document.getElementById("result");
 let timerStarted = false;
 let intervalId;
+let value = 0;
+
+function tick() {
+  value++;
+  result.innerHTML = value;
+}
 
 function startCount() {
-  let value = 0;
-  function count() {
-    value++;
-    result.innerHTML = value;
-  }
   if (!timerStarted) {
     timerStarted = true;
-    intervalId = setInterval(count, 1000);
+    intervalId = setInterval(tick, 1000);
+  } else if (timerStarted) {
+    clearInterval(intervalId);
+    timerStarted = false;
   }
 }
 function stopCount() {
   clearInterval(intervalId);
   timerStarted = false;
 }
+
 startPausa.addEventListener("click", startCount);
 stop.addEventListener("click", stopCount);
