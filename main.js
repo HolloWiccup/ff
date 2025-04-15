b19ac51aabfc288d840d4f5de9b0915bb38f00d6
Ruslan <roslavich16@gmail.com>
2025-04-15 21:06:14 +0300

    добавил логику

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-let resultButton = document.getElementById("resultButton");
-resultButton.addEventListener("click", () => alert("работает"));
+const resultButton = document.getElementById("resultButton");
+
+function calculate() {
+  let result;
+  const operator1 = Number(document.getElementById("operator1").value);
+  const operator2 = Number(document.getElementById("operator2").value);
+  let calculateResult = document.getElementById("calculateResult");
+  let selector = document.getElementById("selector").value;
+  switch (selector) {
+    case "+":
+      result = operator1 + operator2;
+      break;
+    case "-":
+      result = operator1 - operator2;
+      break;
+    case "*":
+      result = operator1 * operator2;
+      break;
+    case "/":
+      result = operator1 / operator2;
+      break;
+  }
+  calculateResult.innerHTML = result;
+}
+resultButton.addEventListener("click", calculate);
